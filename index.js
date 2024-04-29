721ed0efde9710c5641b4712c2b6e26291a694a4
Hana Pavlová <hanapavlova7@gmail.com>
2024-04-29 14:06:59 +0200

    js pro obarveni kosile

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 console.log('funguju');
+
+
+
+const button = document.querySelector(".btn");
+
+button.addEventListener("click", (event) => {
+    
+    const kosile = document.querySelector("#product-image")
+  
+    if (event.target.classList.contains("btn-cerna")) {
+        kosile.style.fill = "#000000";
+    } else if (event.target.classList.contains("btn-cervena")) {
+        kosile.style.fill = "rgb(149,50,50)";
+    } else if (event.target.classList.contains("btn-bila")) {
+        kosile.style.fill = "#ffffff";
+    } else if (event.target.classList.contains("btn-modra")) {
+        kosile.style.fill = "rgb(137,172,204)"
+    }
+})
