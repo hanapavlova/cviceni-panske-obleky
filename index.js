a0f8c93c82ff8c6ffc40e454411369cebd6ca706
Hana Pavlová <hanapavlova7@gmail.com>
2024-05-01 15:40:20 +0200

    uprava js zmeny barvy kosile

--- index.js
+++ index.js
@@ -4,13 +4,27 @@ console.log('funguju');
 
 //const button = document.querySelector(".btn"); > takhle to nejde, protože to vybere jen první s těch selektorů, takže se to musí udělat přes pole a forEach
 
-const buttons = Array.from(document.getElementsByClassName("btn"));
 
+const kosile = document.querySelector("#product-image");
+
+const changeColor = (event) => {
+    const vybranaBarva = event.target.id;
+    const colors = {
+        "color-white": "#ffffff",
+        "color-black": "#000000",
+        "color-red": "rgb(149,50,50)",
+        "color-blue": "rgb(137,172,204)",
+    }
+    kosile.style.fill = colors[vybranaBarva];
+}
+
+const buttons = Array.from(document.getElementsByClassName("btn"));
 buttons.forEach((button) => {
-    button.addEventListener("click", (event) => {
-    
-    const kosile = document.querySelector("#product-image")
-  
+    button.addEventListener("click", changeColor);
+});
+
+
+  /*
     if (event.target.classList.contains("btn-cerna")) {
         kosile.style.fill = "#000000";
     } else if (event.target.classList.contains("btn-cervena")) {
@@ -20,5 +34,9 @@ buttons.forEach((button) => {
     } else if (event.target.classList.contains("btn-modra")) {
         kosile.style.fill = "rgb(137,172,204)"
     }
-})
-})
+*/
+
+
+
+
+
